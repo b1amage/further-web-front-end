1e4f36437daa57c6e53e2034accd4e5040c2d337
whisky-lover <s3877698@rmit.edu.vn>
2022-07-09 09:36:13 +0700

    add router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import NavBar from "./components/navbar/NavBar";
-
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./views/HomePage";
 function App() {
 	return (
 		<>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+			</Routes>
 			<NavBar />
 		</>
 	);
