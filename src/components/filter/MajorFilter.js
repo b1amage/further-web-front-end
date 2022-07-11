ef318c38bc64e5b28223cfbebbc3076e39ec0743
whisky-lover <s3877698@rmit.edu.vn>
2022-07-11 20:56:08 +0700

    caret color

--- src/components/filter/MajorFilter.js
+++ src/components/filter/MajorFilter.js
@@ -14,7 +14,7 @@ const MajorFilter = () => {
 					name="major"
 					type="text"
 					placeholder="Information technology"
-					className="outline-none bg-transparent w-full px-5 py-2 text-[16px] placeholder:text-[16px] text-black border-2 rounded-full placeholder:text-black placeholder:dark:text-white dark:text-white border-primary-100"
+					className="outline-none caret-primary-100 bg-transparent w-full px-5 py-2 text-[16px] placeholder:text-[16px] text-black border-2 rounded-full placeholder:text-black placeholder:dark:text-white dark:text-white border-primary-100"
 				/>
 
 				<div className="w-[28px] absolute right-4 top-1/2 -translate-y-1/2 z-[110]">
