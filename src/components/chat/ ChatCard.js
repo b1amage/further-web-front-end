229084cc93e5ec7a5ccaa84e2a4262ee4f0decd2
whisky-lover <s3877698@rmit.edu.vn>
2022-08-23 14:17:32 +0700

    responsive chat page

--- src/components/chat/ ChatCard.js
+++ src/components/chat/ ChatCard.js
@@ -1,29 +1,35 @@
 import React from "react";
+import SectionTitle from "../../utilities/SectionTitle";
 
 const ChatCard = ({ img, isActive, lastest, name }) => {
 	return (
-		<div className="flex items-center gap-3 p-4 border border-gray rounded-[16px]">
-			<div className="relative w-[60px] h-[60px] rounded-full">
+		<div className="flex cursor-pointer items-center gap-3 md:gap-5 lg:gap-10 p-4 md:p-6 lg:p-8 border border-gray rounded-[16px]">
+			<div className="relative w-[60px] h-[60px] md:w-[80px] md:h-[80px] lg:w-[100px] lg:h-[100px] rounded-full">
 				<img
 					src={img}
 					alt=""
 					className="relative z-10 object-cover w-full h-full rounded-full"
 				/>
-				<div className="absolute bottom-0 right-0 -translate-x-1/2 !z-20 block w-3 h-3 rounded-full bg-gradient-to-br from-primary-50 to-primary-100"></div>
+				{isActive && (
+					<div className="absolute bottom-0 right-0 -translate-x-1/2 !z-20 block w-3 h-3 rounded-full bg-gradient-to-br from-primary-50 to-primary-100"></div>
+				)}
 			</div>
 
 			<div className="flex flex-col flex-1 space-y-2">
-				<h3 className="text-xl font-semibold">{name}</h3>
-				<p className="text-base text-dark-3 dark:text-white">
+				<SectionTitle>{name}</SectionTitle>
+
+				<p className="text-base text-dark-3 dark:text-white lg:text-lg">
 					{lastest.message}
 				</p>
 			</div>
 
 			<div className="flex flex-col items-end space-y-2.5">
-				<p className="w-[25px] h-[25px] rounded-full bg-gradient-to-br from-primary-50 to-primary-100 text-white flex justify-center items-center">
+				<p className="w-[25px] h-[25px] lg:w-[35px] lg:h-[35px] rounded-full bg-gradient-to-br from-primary-50 to-primary-100 text-white flex justify-center items-center">
 					{lastest.messageCount}
 				</p>
-				<p className="text-dark-3 dark:text-white">{lastest.time}</p>
+				<p className="text-dark-3 lg:text-lg dark:text-white">
+					{lastest.time}
+				</p>
 			</div>
 		</div>
 	);
