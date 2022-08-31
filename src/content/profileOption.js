9a64cea72f2be7d94191c28be3688e4e4f744d18
whisky-lover <s3877698@rmit.edu.vn>
2022-08-31 12:34:30 +0700

    add option register + login

--- src/content/profileOption.js
+++ src/content/profileOption.js
@@ -1,12 +1,28 @@
 import profile from "../assets/svg/profile-pink.svg";
 import eye from "../assets/svg/eye.svg";
 import logout from "../assets/svg/logout.svg";
+import register from "../assets/svg/register.svg";
+import login from "../assets/svg/login.svg";
 
 const profileOptions = [
 	{
 		title: "edit profile",
 		icon: profile,
 		to: "/profile/edit",
+		navigatable: true,
+		tooglable: false,
+	},
+	{
+		title: "Register",
+		icon: register,
+		to: "/register",
+		navigatable: true,
+		tooglable: false,
+	},
+	{
+		title: "Login",
+		icon: login,
+		to: "/login",
 		navigatable: true,
 		tooglable: false,
 	},
