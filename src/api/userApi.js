0bb6d4bda8ce436088c0af05927f15b44c316834
Nguyen Luu Quoc Bao <s3877698@rmit.edu.vn>
2022-09-07 18:38:54 +0700

    init user api

---
+++ src/api/userApi.js
@@ -0,0 +1,31 @@
+import axiosClient from "./axiosClient";
+
+const userApi = {
+	async getProfile(id, navigate) {
+		const url = `/user/${id}`;
+		try {
+			const response = await axiosClient.get(url);
+			console.log(response);
+			return response;
+		} catch (error) {
+			console.log(error);
+			// navigate to register fail
+			navigate(`/error/${error.response.data.msg}`);
+		}
+	},
+
+	async deleteUser(id, navigate) {
+		const url = `/user/${id}`;
+		try {
+			const response = await axiosClient.delete(url);
+			console.log(response);
+			return response;
+		} catch (error) {
+			console.log(error);
+			// navigate to register fail
+			navigate(`/error/${error.response.data.msg}`);
+		}
+	},
+};
+
+export default userApi;
