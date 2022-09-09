cb978ebe9403d7b3a5c99cfc20c5812e726612fd
Nguyen Luu Quoc Bao <s3877698@rmit.edu.vn>
2022-09-09 20:33:52 +0700

    fix bug

--- src/views/ChatPage.js
+++ src/views/ChatPage.js
@@ -4,6 +4,8 @@ import SearchBox from "../components/chat/SearchBox";
 import NavBar from "../components/navbar/NavBar";
 import ChatCard from "../components/chat/ ChatCard";
 import girl5 from "../assets/img/girl5.jpeg";
+import authenticationApi from "../api/authenticationApi";
+import NoMore from "../components/main/NoMore";
 
 const ChatPage = () => {
 	return (
@@ -11,23 +13,27 @@ const ChatPage = () => {
 			<Header title="Inbox" />
 			<SearchBox />
 
-			<div className="flex flex-col gap-5">
-				{Array(8)
-					.fill()
-					.map((_, index) => (
-						<ChatCard
-							key={index}
-							img={girl5}
-							isActive
-							name="Wife No.1"
-							lastest={{
-								message: "Let's go for a drink",
-								time: "22:00",
-								messageCount: 2,
-							}}
-						/>
-					))}
-			</div>
+			{!authenticationApi.isLogin() ? (
+				<NoMore />
+			) : (
+				<div className="flex flex-col gap-5">
+					{Array(8)
+						.fill()
+						.map((_, index) => (
+							<ChatCard
+								key={index}
+								img={girl5}
+								isActive
+								name="Wife No.1"
+								lastest={{
+									message: "Let's go for a drink",
+									time: "22:00",
+									messageCount: 2,
+								}}
+							/>
+						))}
+				</div>
+			)}
 
 			<NavBar />
 		</div>
