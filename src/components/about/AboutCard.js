d991f2a61b39345d9dbb6aa1ad1bbc3cb296afad
Nguyen Luu Quoc Bao <s3877698@rmit.edu.vn>
2022-09-14 10:04:05 +0700

    refactor

--- src/components/about/AboutCard.js
+++ src/components/about/AboutCard.js
@@ -1,8 +1,7 @@
-import React from "react";
-
 const AboutCard = ({ name, img, role, fb }) => {
 	return (
 		<a href={fb} target="_blank" rel="noreferrer">
+			{/* IMAGE */}
 			<div className="cursor-pointer w-[300px] shadow-2xl h-[300px] lg:w-[400px] lg:h-[400px] rounded-[36px] overflow-hidden relative">
 				<img
 					src={img}
@@ -10,6 +9,7 @@ const AboutCard = ({ name, img, role, fb }) => {
 					className="object-cover w-full h-full transition-all duration-300 hover:scale-125"
 				/>
 
+				{/* INFO SECTION */}
 				<div className="flex-col p-10 gap-2 absolute bottom-0 left-0 z-20 flex justify-center text-white w-full bg-gradient-to-b from-transparent to-primary-100 dark:to-primary-80 transiton-all duration-300 h-2/5 rounded-[36px]">
 					<h3 className="text-4xl font-semibold capitalize">
 						{name}
